17a7d252d8d2a9c56e9bc5dfc15d83cf325bbd29
stardiviner <numbchild@gmail.com>
2020-06-21 23:41:07 +0800

    add defcustom options and generate API functions core macro

--- syncthing.el
+++ syncthing.el
@@ -1,6 +1,6 @@
 ;;; syncthing.el --- Syncthing client for Emacs -*- lexical-binding: t; -*-
 
-;;; Time-stamp: <2020-06-21 21:27:00 stardiviner>
+;;; Time-stamp: <2020-06-21 23:36:39 stardiviner>
 
 ;; Authors: stardiviner <numbchild@gmail.com>
 ;; Package-Requires: ((emacs "25.1"))
@@ -28,8 +28,42 @@
 
 ;;; Code:
 
+(defgroup syncthing nil
+  "Syncthing client customize group."
+  :prefix "syncthing-"
+  :group 'syncthing)
+
+(defcustom syncthing-server "http://localhost"
+  "The server of Syncthing."
+  :type 'string
+  :safe #'stringp
+  :group 'syncthing)
+
+(defcustom syncthing-port 8080
+  "The port of Syncthing."
+  :type 'number
+  :safe #'numberp
+  :group 'syncthing)
+
+(defcustom syncthing-api-key nil
+  "The API key for Syncthing."
+  :type 'string
+  :safe #'stringp
+  :group 'syncthing)
 
 
+(defmacro syncthing--rest-api (method endpoint &rest arglist)
+  "The Syncthing REST API macro to construct HTTP METHOD, ENDPOINT with ARGLIST."
+  `(defun ,(intern (format "syncthing-api-%s-%s" method endpoint)) ,arglist
+     (let ((url-request-method ,method)
+           (url-request-extra-headers '(("X-API-Key" . ,syncthing-api-key))))
+       (with-current-buffer
+           (url-retrieve-synchronously
+            (format "%s:%s%s" syncthing-server syncthing-port ,endpoint))
+         (goto-char (point-min))
+         (re-search-forward "^$")
+         (let ((result (json-read)))
+           result)))))
 
 
 
