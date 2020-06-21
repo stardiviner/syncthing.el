fd77ad7c23665ff85d43435318b5d96985760e2a
stardiviner <numbchild@gmail.com>
2020-06-21 21:27:20 +0800

    first initial commit

---
+++ syncthing.el
@@ -0,0 +1,39 @@
+;;; syncthing.el --- Syncthing client for Emacs -*- lexical-binding: t; -*-
+
+;;; Time-stamp: <2020-06-21 21:27:00 stardiviner>
+
+;; Authors: stardiviner <numbchild@gmail.com>
+;; Package-Requires: ((emacs "25.1"))
+;; Package-Version: 0.1
+;; Keywords: comm
+;; homepage: https://github.com/stardiviner/syncthing.el
+
+;; syncthing.el is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; syncthing.el is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+;;
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Usage: [M-x syncthing]
+
+;;; Code:
+
+
+
+
+
+
+
+(provide 'syncthing)
+
+;;; syncthing.el ends here
